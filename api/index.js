4fcae11697498dd8fd73eb46cbb06c3ac83ab3ac
Abel Masresha <abelmasre@gmail.com>
2024-08-05 18:08:48 +0300

    Added Create a post api functionality

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,21 +22,20 @@ mongoose
     console.log(err);
   });
 
-
-
 app.listen(3000, () => {
   console.log("Server is Running on port 3000 !!");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
-app.use((err, req, res, next)=>{
-  const statusCode = err.statusCode || 500
-  const message = err.message || 'Internal Server Errror'
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Errror";
   res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
-  })
-})
+  });
+});
