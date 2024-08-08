6cf690c21d62d6d68e393555707b49ddfd0430d5
Abel Masresha <abelmasre@gmail.com>
2024-08-08 14:19:17 +0300

    Added About and Project Pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
     <div>
-      
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-2xl mx-auto p-3 text-center">
+          <h1 className="text-3xl font font-semibold text-center my-7">About Jireh Tech</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Jireh Tech is an emerging technology startup with a bold vision to
+              transform the embedded technology landscape in Ethiopia. Founded
+              on a commitment to excellence, passion, and solution-driven
+              innovation, our company is dedicated to pioneering embedded
+              technology solutions that address the unique challenges faced by
+              Ethiopian industries and communities. Our name, "Jireh," derived
+              from Hebrew, signifies our commitment to providing solutions that
+              bring prosperity and abundance to our customers and stakeholders
+            </p>
+            <p>
+              Core Values: Excellence: We are committed to excellence in
+              everything we do, from conceptualizing ideas to executing
+              projects, setting high standards for quality and performance.
+              Passion: Our passion for technology and social impact drives our
+              ambition and fuels our creativity, inspiring us to pursue bold
+              ideas and overcome challenges. Solution-Oriented: We are
+              solution-oriented, dedicated to finding practical and sustainable
+              solutions to address real-world problems, leveraging technology as
+              a force for positive change.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
